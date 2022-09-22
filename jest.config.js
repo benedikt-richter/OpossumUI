84d54e1bee181b3be2bd983153ca752eef5668f8
Benedikt Richter <benedikt.richter@tngtech.com>
2022-09-22 18:25:11 +0200

    Add test coverage
    Signed-off-by: Benedikt Richter <benedikt.richter@tngtech.com>

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,11 @@
 // SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
 //
 // SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
+};
